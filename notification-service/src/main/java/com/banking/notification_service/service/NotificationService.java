2b920d53603cdcc4c6a909c5cd18866f6048aeaf
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-08-02 23:33:04 +0200

    ADDED kafka consumers for to send event alerts IN NOTIFICATION SERVICE

--- notification-service/src/main/java/com/banking/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/banking/notification_service/service/NotificationService.java
@@ -41,6 +41,118 @@ public class NotificationService {
         }
     }
 
+    @KafkaListener(topics = "transaction.completed")
+    public void consumeTransactionCompleted(
+            @Payload Map<String, Object> payload
+    )
+    {
+        try{
+            String senderAccount = payload.get("senderAccountNumber").toString();
+            String receiverAccount = payload.get("receiverAccountNumber").toString();
+            String amount = payload.get("amount").toString();
+
+            //debit alert
+            sendAlert(senderAccount,
+                    "DEBIT ALERT",
+                    String.format("%s debited from account %s", amount, senderAccount)
+                    );
+
+            //credit alert
+            sendAlert(receiverAccount,
+                    "CREDIT ALERT",
+                    String.format("%s credit into account %s", amount, receiverAccount)
+                    );
+        }
+        catch(Exception e){
+            log.error("Error sending transaction alerts: {}", e.getMessage());
+
+    }
+    }
+
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(
+            @Payload Map<String, Object> payload
+    )
+    {
+        try{
+            String accountNumber = payload.get("accountNumber").toString();
+            String reason = payload.get("reason").toString();
+
+            //fraud alert
+            sendAlert(accountNumber,
+                    "Suspicious activity detected",
+                    String.format("Your account %s blocked. Reason: %s", accountNumber, reason)
+            );
+        }
+        catch(Exception e){
+            log.error("Error sending fraud alert: {}", e.getMessage());
+
+        }
+
+    }
+
+    @KafkaListener(topics = "transaction.refunded")
+    public void consumeTransactionRefunded(
+            @Payload Map<String, Object> payload
+    )
+    {
+        try{
+            String accountNumber = payload.get("senderAccountNumber").toString();
+            String reason = payload.get("reason").toString();
+            String amount = payload.get("amount").toString();
+
+            //fraud alert
+            sendAlert(accountNumber,
+                    "REFUND PROCESSED",
+                    String.format("Your transaction from account number %s was unsuccessful. Refunded amount: %s. Reason: %s",
+                            accountNumber, amount, reason)
+            );
+        }
+        catch(Exception e){
+            log.error("Error sending refund alert: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "payment.completed")
+    public void consumePaymentCompleted(
+            @Payload Map<String, Object> payload
+    )
+    {
+        try{
+            String accountNumber = payload.get("accountNumber").toString();
+            String amount = payload.get("amount").toString();
+
+            //sender alert
+            sendAlert(accountNumber,
+                    "PAYMENT SUCCESSFUL",
+                    String.format("Your payment from account number %s was successful. Deducted amount: %s.", accountNumber, amount)
+            );
+        }
+        catch(Exception e){
+            log.error("Error sending payment alert: {}", e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics = "payment.failed")
+    public void consumePaymentFailed(
+            @Payload Map<String, Object> payload
+    )
+    {
+        try{
+            String accountNumber = payload.get("accountNumber").toString();
+            String amount = payload.get("amount").toString();
+
+            //payment failed alert
+            sendAlert(accountNumber,
+                    "PAYMENT FAILED",
+                    String.format("Your payment from account number %s was unsuccessful of amount: %s.", accountNumber, amount)
+            );
+        }
+        catch(Exception e){
+            log.error("Error sending failed payment alert: {}", e.getMessage());
+        }
+    }
+
     private void sendAlert(String accountNumber, String subject, String message) {
 
 
