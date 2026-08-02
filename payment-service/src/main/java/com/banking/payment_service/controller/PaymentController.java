d4ab4f2afba92ad503147f77f0d00943320c51c1
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-08-02 16:41:46 +0200

    ADDED controllers IN Payment SERVICE

--- payment-service/src/main/java/com/banking/payment_service/controller/PaymentController.java
+++ payment-service/src/main/java/com/banking/payment_service/controller/PaymentController.java
@@ -1,10 +1,19 @@
 package com.banking.payment_service.controller;
 
+import com.banking.payment_service.dto.CreatePaymentRequestDTO;
+import com.banking.payment_service.dto.PaymentOrderResponseDTO;
 import com.banking.payment_service.service.PaymentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -13,4 +22,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class PaymentController {
 
     private final PaymentService paymentService;
+
+    @PostMapping("/create-order")
+    public ResponseEntity<PaymentOrderResponseDTO> createPaymentOrder(
+            @Valid @RequestBody CreatePaymentRequestDTO requestDTO
+            )
+    {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(paymentService.createPaymentOrder(requestDTO));
+    }
+
+    // Razorpay
+    @PostMapping("/webhook")
+    public ResponseEntity<String> handleWebhook(
+            @RequestBody Map<String, Object> payload
+            )
+    {
+        paymentService.handleWebhook(payload);
+
+        return ResponseEntity.ok("Webhook processed")
+    }
 }
