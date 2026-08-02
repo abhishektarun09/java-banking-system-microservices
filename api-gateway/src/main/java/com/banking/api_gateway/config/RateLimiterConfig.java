252c52164f5dc9e2b60ed76388bd298142a89453
Abhishek Tarun <abhishek.tarun09@gmail.com>
2026-08-02 18:42:03 +0200

    ADDED redis rate limiter (Token Bucket algorithm) IN api-gateway

---
+++ api-gateway/src/main/java/com/banking/api_gateway/config/RateLimiterConfig.java
@@ -0,0 +1,24 @@
+package com.banking.api_gateway.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public KeyResolver keyResolver() {
+
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest()
+                                .getRemoteAddress())
+                        .getAddress()
+                        .getHostAddress()
+        );
+
+    }
+}
